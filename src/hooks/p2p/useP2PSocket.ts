ca99bfea50b5d2d5f7126987d0cd8e785bc1550d
Arslan Mahesar <mahesararslan1998@gmail.com>
2025-07-24 11:58:31 +0500

    Tests WS_URL bug 2

--- src/hooks/p2p/useP2PSocket.ts
+++ src/hooks/p2p/useP2PSocket.ts
@@ -4,7 +4,8 @@ import { useToast } from '@/src/lib/ToastProvider';
 import { useState, useEffect, useCallback, useRef } from "react";
 import { io, Socket } from "socket.io-client";
 
-const SOCKET_URL = process.env.NEXT_PUBLIC_WS_URL || "";
+// const SOCKET_URL = process.env.NEXT_PUBLIC_WS_URL || "";
+const SOCKET_URL = 'http://34.75.109.160:3002';
 
 export const useP2PSocket = () => {
   const [isConnected, setIsConnected] = useState(false);
